b020166a26553bb560f7f0c7abd303657f0d5822
sanjosechristinejoypdm-creator <sanjosechristinejoy.pdm@gmail.com>
2026-09-20 15:10:07 +0800

    Update API_URL to new endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:5000";
-const API_URL = "https://frontend-xi-jet-79.vercel.app/";
+const API_URL = "https://login-system-97ea.onrender.com";
 
 async function register() {
   const name = document.getElementById("name").value;
